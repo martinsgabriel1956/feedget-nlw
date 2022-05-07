569555b429e53fb4c57a4e3d7e3c3974e7a3e18d
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-05-07 16:44:11 -0300

    feat: adding screenshot in the email

--- server/src/UseCases/SubmitFeedbackUseCase.ts
+++ server/src/UseCases/SubmitFeedbackUseCase.ts
@@ -37,8 +37,9 @@ export class SubmitFeedbackUseCase {
         `<div style="font-family: sans-serif; font-size: 16px color: #111;">`,
           `<p>Tipo do feedback: ${type}</p>`,
           `<p>Comentário: ${comment}</p>`,
+          screenshot ? `<img w src="${screenshot}" />` : ``,
         `</div>`
-      ].join('')
+      ].join('\n')
     })
   }
 }
