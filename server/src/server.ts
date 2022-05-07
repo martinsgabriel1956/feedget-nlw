5bb1ccc50811099be10643dfb8f3baaed7641d2e
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-05-07 19:04:07 -0300

    fix: adding limit

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {  } from 'express';
 import cors from 'cors';
 
 import { routes } from './routes';
@@ -6,6 +6,7 @@ import { routes } from './routes';
 const app = express();
 
 app.use(cors())
+app.use(express.urlencoded({limit: '3mb'}));
 app.use(express.json());
 app.use(routes);
 
